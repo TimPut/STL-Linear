29531d9045f1591f8da750e98ac030c5130fd335
timput <tim@timput.com>
2021-05-29 14:35:53 -0600

    remove benchmarking stl
    we previously bundled the 3DBenchy for benchmarking. We will now require devs to manually download an stl with which to benchmark to avoid forcing hackage to hsot blobs.

--- benchmark/Main.hs
+++ benchmark/Main.hs
@@ -4,42 +4,44 @@
 {-# LANGUAGE StandaloneDeriving #-}
 module Main (main) where
 
-import           Data.Attoparsec.ByteString
-import qualified Data.Attoparsec.Text       as T
+import           Data.Attoparsec.ByteString hiding (try)
+-- import qualified Data.Attoparsec.Text       as T
 import           Data.Binary
 import qualified Data.ByteString            as B
 import           Data.ByteString.Lazy       (fromStrict)
-import qualified Data.Text                  as T
+-- import qualified Data.Text                  as T
 import qualified Data.Text.IO               as T
 import           Gauge
-import qualified Graphics.Formats.STL       as F
+--import qualified Graphics.Formats.STL       as F
 import           Graphics.STL
 
-import           Control.DeepSeq
-import           GHC.Generics               (Generic)
-
+-- import           Control.DeepSeq
+-- import           GHC.Generics               (Generic)
+import           Control.Exception
 
 main :: IO ()
 main = do
-  stl <- B.readFile "./benchmark/3DBenchy.stl"
-  let r = decode (fromStrict stl) :: STL
-  B.writeFile "./benchmark/ASCII3DBenchy.stl" (unparseSTL r)
-  defaultMainWith defaultConfig
-       [ env setupEnv $
-       \ ~(ascii,binary,text) -> bgroup "main"
-                           [ bgroup "ascii" [
-                                         bench "STL-Linear parse"
-                                         (nf asciiStlLinear ascii)
-                                        , bench "STL parse"
-                                         (nf asciiStl text)
+  file <- try (B.readFile "./benchmark/3DBenchy.stl") :: IO (Either SomeException B.ByteString)
+  case file of
+    Left ex -> putStrLn "Could not find ./benchmark/3DBenchy.stl, aborting benchmarking"
+    Right stl -> do
+      let r = decode (fromStrict stl) :: STL
+      B.writeFile "./benchmark/ASCII3DBenchy.stl" (unparseSTL r)
+      defaultMainWith (defaultConfig {quickMode = True})
+        [ env setupEnv $
+          \ ~(ascii,binary,text) -> bgroup "main"
+                                   [ bgroup "ascii" [
+                                       bench "STL-Linear parse"
+                                       (nf asciiStlLinear ascii)
+                                       -- , bench "STL parse"
+                                       -- (nf asciiStl text)
                                        ]
-                           , bgroup "binary" [
-                                  bench "STL-Linear parse"
-                                  (nf binaryStl binary)
-                                 ]
-                           ]
-         ]
-
+                                   , bgroup "binary" [
+                                       bench "STL-Linear parse"
+                                       (nf binaryStl binary)
+                                       ]
+                                   ]
+        ]
 
 setupEnv = do
   asciiSTL <- B.readFile "./benchmark/ASCII3DBenchy.stl"
@@ -53,10 +55,10 @@ binaryStl bs = decode (fromStrict bs)
 asciiStlLinear ::  B.ByteString -> STL
 asciiStlLinear bs = let Right r = parseOnly parseSTL bs in r
 
-asciiStl :: T.Text -> F.STL
-asciiStl ts = let Right r = T.parseOnly F.stlParser ts in r
+--asciiStl :: T.Text -> F.STL
+--asciiStl ts = let Right r = T.parseOnly F.stlParser ts in r
 
-deriving instance Generic (F.Triangle)
-deriving instance NFData (F.Triangle)
-deriving instance Generic (F.STL)
-deriving instance NFData (F.STL)
+-- deriving instance Generic (F.Triangle)
+-- deriving instance NFData (F.Triangle)
+-- deriving instance Generic (F.STL)
+-- deriving instance NFData (F.STL)
