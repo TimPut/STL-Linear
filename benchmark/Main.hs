b41068fadf915b48c4cf3a3c70833e6aac07b7b1
timput <tim@timput.com>
2021-05-29 23:25:22 -0600

    purge STL library and refactor for lazy strict split

--- benchmark/Main.hs
+++ benchmark/Main.hs
@@ -1,64 +1,67 @@
-{-# LANGUAGE DeriveAnyClass     #-}
-{-# LANGUAGE DeriveGeneric      #-}
-
-{-# LANGUAGE StandaloneDeriving #-}
 module Main (main) where
 
+import           Control.Exception
 import           Data.Attoparsec.ByteString hiding (try)
--- import qualified Data.Attoparsec.Text       as T
 import           Data.Binary
 import qualified Data.ByteString            as B
 import           Data.ByteString.Lazy       (fromStrict)
--- import qualified Data.Text                  as T
-import qualified Data.Text.IO               as T
 import           Gauge
---import qualified Graphics.Formats.STL       as F
 import           Graphics.STL
-
--- import           Control.DeepSeq
--- import           GHC.Generics               (Generic)
-import           Control.Exception
+import qualified Graphics.STL.Lazy as L
 
 main :: IO ()
 main = do
-  file <- try (B.readFile "./benchmark/3DBenchy.stl") :: IO (Either SomeException B.ByteString)
+  file <- try (B.readFile binaryStlPath) :: IO (Either SomeException B.ByteString)
   case file of
-    Left ex -> putStrLn "Could not find ./benchmark/3DBenchy.stl, aborting benchmarking"
+    Left _ -> putStrLn $ "Could not find " ++ binaryStlPath ++ ", aborting benchmarking"
     Right stl -> do
       let r = decode (fromStrict stl) :: STL
-      B.writeFile "./benchmark/ASCII3DBenchy.stl" (unparseSTL r)
+      B.writeFile asciiStlPath (unparseSTL r)
       defaultMainWith (defaultConfig {quickMode = True})
         [ env setupEnv $
-          \ ~(ascii,binary,text) -> bgroup "main"
-                                   [ bgroup "ascii" [
-                                       bench "STL-Linear parse"
-                                       (nf asciiStlLinear ascii)
-                                       -- , bench "STL parse"
-                                       -- (nf asciiStl text)
-                                       ]
-                                   , bgroup "binary" [
-                                       bench "STL-Linear parse"
-                                       (nf binaryStl binary)
-                                       ]
-                                   ]
+          \ ~(ascii,binary) -> bgroup "main"
+                              [ bgroup "ascii" [
+                                  bench "STL-Linear parse"
+                                    (nf asciiStl' ascii)
+                                  , bench "STL-Linear lazy parse"
+                                    (nf asciiStl ascii)
+                                  ]
+                              , bgroup "binary" [
+                                  bench "STL-Linear parse"
+                                    (nf binaryStl' binary)
+                                  , bench "STL-Linear lazy parse"
+                                    (nf binaryStl binary)
+                                  , bench "STL-Linear lazy header only parse"
+                                    (nf (L.header . binaryStl) binary)
+
+                                  ]
+                              ]
         ]
 
-setupEnv = do
-  asciiSTL <- B.readFile "./benchmark/ASCII3DBenchy.stl"
-  binarySTL <- B.readFile "./benchmark/3DBenchy.stl"
-  textSTL <- T.readFile "./benchmark/ASCII3DBenchy.stl"
-  return (asciiSTL, binarySTL, textSTL)
+asciiStlPath, binaryStlPath :: String
+asciiStlPath = "./models/ASCII.stl"
+-- Replace this model with something heavier, like the 3DBenchy for
+-- meaningful benchmarks
+binaryStlPath = "./models/Binary.stl"
 
-binaryStl :: B.ByteString -> STL
+setupEnv :: IO (B.ByteString, B.ByteString)
+setupEnv = do
+  asciiSTL <- B.readFile asciiStlPath
+  binarySTL <- B.readFile binaryStlPath
+  return (asciiSTL, binarySTL)
+
+binaryStl' :: B.ByteString -> STL
+binaryStl' bs = decode (fromStrict bs)
+
+binaryStl :: B.ByteString -> L.STL
 binaryStl bs = decode (fromStrict bs)
 
-asciiStlLinear ::  B.ByteString -> STL
-asciiStlLinear bs = let Right r = parseOnly parseSTL bs in r
+asciiStl' ::  B.ByteString -> STL
+asciiStl' bs = case parseOnly parseSTL bs of
+                 Left e -> error e
+                 Right r -> r
 
---asciiStl :: T.Text -> F.STL
---asciiStl ts = let Right r = T.parseOnly F.stlParser ts in r
-
--- deriving instance Generic (F.Triangle)
--- deriving instance NFData (F.Triangle)
--- deriving instance Generic (F.STL)
--- deriving instance NFData (F.STL)
+asciiStl ::  B.ByteString -> L.STL
+asciiStl bs = case parseOnly L.parseSTL bs of
+                 Left e -> error e
+                 Right r -> r
